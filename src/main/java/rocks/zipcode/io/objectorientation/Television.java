a7ebeeb4c2d489f8f3f65ad8df8f158c5e76d598
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-16 10:48:09 -0400

    Finished television except negative set channel

--- src/main/java/rocks/zipcode/io/objectorientation/Television.java
+++ src/main/java/rocks/zipcode/io/objectorientation/Television.java
@@ -1,13 +1,32 @@
 package rocks.zipcode.io.objectorientation;
 
 public class Television {
+    private Boolean isOn;
+    private TVChannel channel;
+
+    public Television(){
+        this.isOn = false;
+        this.channel = TVChannel.MSN;
+    }
+
+    public Television(Boolean isOn, TVChannel channel){
+        this.isOn = isOn;
+        this.channel = channel;
+    }
+
     public void turnOn() {
+        this.isOn = true;
     }
 
     public void setChannel(Integer channel) {
+        if (isOn = true) {
+            this.channel = TVChannel.getByOrdinal(channel);
+        } else {
+            throw new IllegalStateException("TV is off");
+        }
     }
 
     public TVChannel getChannel() {
-        return null;
+        return channel;
     }
 }
