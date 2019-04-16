7724bdcda5a6ebafe9f1fe4102817922ab2fc556
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-16 10:52:57 -0400

    Finished generic utils

--- src/main/java/rocks/zipcode/io/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/generics/GenericUtils.java
@@ -9,6 +9,12 @@ public class GenericUtils {
      * @return array with contents identical to `collection`
      */
     public static <T> T[] toArray(Collection<T> collection) {
-        return null;
+        T[] genericArray = (T[])new Object[collection.size()];
+        int count = 0;
+        for (T t : collection){
+            genericArray[count] = t;
+            count++;
+        }
+        return genericArray;
     }
 }
