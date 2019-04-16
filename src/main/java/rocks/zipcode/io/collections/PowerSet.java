a6891cf879414d1ff49590533f77f7ddf26191f0
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-16 11:34:54 -0400

    Finished collections power set

--- src/main/java/rocks/zipcode/io/collections/PowerSet.java
+++ src/main/java/rocks/zipcode/io/collections/PowerSet.java
@@ -1,8 +1,6 @@
 package rocks.zipcode.io.collections;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author leon on 10/01/2019.
@@ -18,7 +16,26 @@ public class PowerSet<TypeOfSet> {
      * @return the powerset of `originalSet`
      */
     public Set<Set<TypeOfSet>> permute() {
-        return null;
+        return powerSet(originalSet);
+    }
+
+    public Set<Set<TypeOfSet>> powerSet(Set<TypeOfSet> originalSet){
+        Set<Set<TypeOfSet>> sets = new HashSet<>();
+        if (originalSet.isEmpty()) {
+            sets.add(new HashSet<>());
+            return sets;
+        }
+        List<TypeOfSet> list = new ArrayList<>(originalSet);
+        TypeOfSet head = list.get(0);
+        Set<TypeOfSet> rest = new HashSet<>(list.subList(1, list.size()));
+        for (Set<TypeOfSet> set : powerSet(rest)){
+            Set<TypeOfSet> newSet = new HashSet<>();
+            newSet.add(head);
+            newSet.addAll(set);
+            sets.add(newSet);
+            sets.add(set);
+        }
+        return sets;
     }
 
     /**
